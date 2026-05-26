1a1a7e10c26da2e7746453105714f9b0858c3d85
ScPro <muhamadievbogdan@gmai.com>
2026-05-26 20:49:57 +0500

    update main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,19 @@
-package main
+﻿package main
 
-import "fmt"
+import (
+	"github.com/0ScPro0/go-todolist/internal/core/config"
+	"github.com/0ScPro0/go-todolist/internal/core/logger"
+)
 
 func main() {
-	fmt.Println("Go-todolist app")
+	cfg, err := config.NewConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	log, err := logger.NewLogger(cfg)
+	if err != nil {
+		panic(err)
+	}
+	log.Info("Logger successfully initialized")
 }
