00cd31086123e1cb54308dbb91b66a12229d827d
ScPro <muhamadievbogdan@gmai.com>
2026-05-26 20:50:33 +0500

    add task domain entity

---
+++ internal/core/domain/task.go
@@ -0,0 +1,26 @@
+package domain
+
+// TaskStatus type
+type TaskStatus int
+
+const (
+	NEW TaskStatus = iota
+	IN_PROGRESS
+	DONE
+)
+
+func (s TaskStatus) String() string {
+	return [...]string{"new", "in_progress", "done"}[s]
+}
+
+// Domain entity Task
+// Task has required fields Name and Status
+// and optional field Description
+type Task struct {
+	ID      int `json:"id"`
+	Version int `json:"version"`
+
+	Name        string     `json:"name"`
+	Description *string    `json:"description"`
+	Status      TaskStatus `json:"status"`
+}
